f5cdea96b270ee4792f988ef774a64863ba0879d
qiaoyi213 <linqiaoyi213@gmail.com>
2025-01-25 14:52:33 +0800

    feat: Add NDrawer

--- src/components/feeds.vue
+++ src/components/feeds.vue
@@ -1,5 +1,6 @@
 <script>
-import { NCard, NList, NListItem, NScrollbar} from 'naive-ui'
+import { NCard, NList, NListItem, NScrollbar, NDrawer, NDrawerContent} from 'naive-ui'
+import { ref } from 'vue';
 function greet(event) {
   alert(`Hello ${name.value}!`)
   // `event` 是 DOM 原生事件
@@ -12,30 +13,39 @@ export default {
         NCard,
         NList,
         NListItem,
-        NScrollbar
+        NScrollbar,
+        NDrawer,
+        NDrawerContent
     },
     data() {
         return {
             card_title: "abc",
             feeds_list: [
                 {
-                    "title": "First Feed",
-                    "abstract": "This is the first feed",
-                    "content": "The content, first feed",
+                    title: "First Feed",
+                    abstract: "This is the first feed",
+                    source: "https://abc.com",
+                    content: "The content, first feed",
                 }, 
                 {
-                    "title": "Second Feed",
-                    "abstract": "This is the second feed",
-                    "content": "The content, second feed"
+                    title: "Second Feed",
+                    abstract: "This is the second feed",
+                    source: "https://cde.com",
+                    content: "The content, second feed",
+
                 }
             ]
         }
     },
     setup() {
+        const active = ref(false);
+        const read_feed = (feed_url) => {
+                // channel = invoke('getFeedByUrl', feed_url);
+                active.value = true;
+        }
         return {
-            greet() {
-                console.log("CLICK CARD")
-            }
+            active,
+            read_feed
         }
     }
 }
@@ -45,14 +55,19 @@ export default {
 <template>
         <n-list hoverable clickable>
             <n-list-item v-for="feed in feeds_list">
-                <n-card v-bind:title="feed.title" style="width:500px;"@click="greet">
+                <n-card v-bind:title="feed.title" style="width:500px;" @click="read_feed(feed.title)">
                     <template #cover>
                         <img class="image" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.thoughtco.com%2Fthmb%2Fe6FEm0_xDZ_kiajFKMFrBO6hgb0%3D%2F1500x0%2Ffilters%3Ano_upscale()%3Amax_bytes(150000)%3Astrip_icc()%2F5925354646_581f193d2c_o-56a28a335f9b58b7d0cbebe0.jpg&f=1&nofb=1&ipt=d8a5dbab505e5f9caae7952d248356172c1547b8a09fde16b1e975c0fca349ab&ipo=images" />
                     </template>
-                    {{ feed. abstract }}
+                    {{ feed.abstract }}
                 </n-card>
             </n-list-item>
         </n-list>
+        <n-drawer v-model:show="active" :width="502" :placement="right">
+            <n-drawer-content title="Feed" closable>
+                ABC
+            </n-drawer-content>
+        </n-drawer>
 </template>
 
 <style>
